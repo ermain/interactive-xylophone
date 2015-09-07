d24f57c1c888ddfeb25d4bd2921cf4ef8f0d57fe
Erin Main <ermain@mit.edu>
2015-09-07 15:28:09 -0400

    move stuff around, working version of xylophone.ino

--- arduino/xylophone/xylophone.ino
+++ arduino/xylophone/xylophone.ino
@@ -1,30 +1,73 @@
+#include <Adafruit_NeoPixel.h>
+#define LED_PIN 6 
+#define LED_COUNT 1
+
+// Create an instance of the Adafruit_NeoPixel class called "leds".
+// That'll be what we refer to from here on...
+Adafruit_NeoPixel leds = Adafruit_NeoPixel(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
+
 // these constants won't change:
-const int ledPin = 13;      // led connected to digital pin 13
-const int knockSensor = A0; // the piezo is connected to analog pin 0
+const int mux_sig_pin = A0; // the piezo is connected to analog pin 0
 const int threshold = 100;  // threshold value to decide when the detected sound is a knock or not
 
+int mux_ctrl_pins[] = {2, 3, 4, 5};
+int sensorReading = 0;
 
 // these variables will change:
-int sensorReading = 0;      // variable to store the value read from the sensor pin
-int ledState = LOW;         // variable used to store the last LED status, to toggle the light
+int key_state[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};      // variable to store the value read from the sensor pin
+
+// function declaration
+int readMux(int chan);     // reads from the analog mux
+int flashLight(int led_num);
 
 void setup() {
- pinMode(ledPin, OUTPUT); // declare the ledPin as as OUTPUT
- Serial.begin(9600);       // use the serial port
+  for(int i = 0; i < 4; i++){
+    pinMode(mux_ctrl_pins[i], OUTPUT);
+    digitalWrite(mux_ctrl_pins[i], LOW);
+  }
+  leds.begin();
+  Serial.begin(9600);       // use the serial port
+  leds.setPixelColor(0, 0xFFFFFF);
+  leds.show();
 }
 
 void loop() {
   // read the sensor and store it in the variable sensorReading:
-  sensorReading = analogRead(knockSensor);    
-  
-  // if the sensor reading is greater than the threshold:
-  if (sensorReading >= threshold) {
-    // toggle the status of the ledPin:
-    ledState = !ledState;   
-    // update the LED pin itself:        
-    digitalWrite(ledPin, ledState);
-    // send the string "Knock!" back to the computer, followed by newline
-    Serial.println("Knock!");         
+  for(int i = 0; i < 13; i++) {
+    sensorReading = readMux(i);    
+   // Serial.print("sensor: ");
+   // Serial.print(i);
+   // Serial.print(" - ");
+   // Serial.print(sensorReading); 
+    // if the sensor reading is greater than the threshold:
+    if (sensorReading >= threshold) {
+      // send the string "Knock!" back to the computer, followed by newline
+      key_state[i] = !key_state[i];
+      Serial.print("hit key: ");
+      Serial.println(i);
+      Serial.print("sensor val: ");
+      Serial.println(sensorReading);
+      //flashLight(i);
+    }
   }
-  delay(100);  // delay to avoid overloading the serial port buffer
+}
+
+int flashLight(int led_num) {
+  if(key_state[led_num] == 1)
+    leds.setPixelColor(led_num, 0xFF00FF);
+  else
+    leds.setPixelColor(led_num, 0x00FF00);
+  leds.show();
+}
+
+// Read a value from the mux.
+int readMux(int chan) {
+  for(int i = 0; i < 4; i++){
+    if((chan >> i) & 0x0001 == 1)
+      digitalWrite(mux_ctrl_pins[i], HIGH);
+    else
+      digitalWrite(mux_ctrl_pins[i], LOW);
+  }
+  delayMicroseconds(1);
+  return analogRead(mux_sig_pin);
 }
